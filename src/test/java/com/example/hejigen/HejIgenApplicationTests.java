c9a52753c52360c4a8edc2b9c524b5aa077db094
Gustavseriksen <guer0001@stud.kea.dk>
2025-02-18 11:05:27 +0100

    Hello

---
+++ src/test/java/com/example/hejigen/HejIgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hejigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HejIgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
